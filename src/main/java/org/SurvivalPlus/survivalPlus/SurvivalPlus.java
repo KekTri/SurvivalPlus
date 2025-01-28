bbadc45047ae8df1f219ab6b24f5541dd89192b0
TimoBuuuu <86928438+TimoBuuuu@users.noreply.github.com>
2025-01-28 20:14:16 +0100

    neue funktionen

--- src/main/java/org/SurvivalPlus/survivalPlus/SurvivalPlus.java
+++ src/main/java/org/SurvivalPlus/survivalPlus/SurvivalPlus.java
@@ -1,22 +1,25 @@
 package org.SurvivalPlus.survivalPlus;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SurvivalPlus extends JavaPlugin {
+public final class SurvivalPlus extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
-    public void test(){
 
-    }
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
-    public static void test2 () {
-        //
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Hallo!");
     }
 }
