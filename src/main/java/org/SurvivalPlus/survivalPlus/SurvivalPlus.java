e14e097980fb33e5bc7aeb60826fc90b8655651d
Kevin_Tribus <kekderbaumeister@gmail.com>
2025-01-29 08:29:17 +0100

    StickJump und PigBreed Glow hinzugefügt

--- src/main/java/org/SurvivalPlus/survivalPlus/SurvivalPlus.java
+++ src/main/java/org/SurvivalPlus/survivalPlus/SurvivalPlus.java
@@ -1,9 +1,25 @@
 package org.SurvivalPlus.survivalPlus;
 
+import org.bukkit.Instrument;
+import org.bukkit.Material;
+import org.bukkit.Note;
+import org.bukkit.Sound;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityBreedEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemType;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+import java.util.Objects;
+
 
 public final class SurvivalPlus extends JavaPlugin implements Listener {
 
@@ -21,5 +37,34 @@ public final class SurvivalPlus extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.getPlayer().sendMessage("Hallo!");
+        event.getPlayer().playSound(event.getPlayer().getLocation(),Sound.BLOCK_BELL_USE,1.0F,1.0F);
     }
+    @EventHandler
+    public void onBreed(EntityBreedEvent event) {
+        if (event.getEntity().getType() == EntityType.PIG) {
+            event.getEntity().setGlowing(true);
+        }
+    }
+    @EventHandler
+    public void stickJump(PlayerInteractEvent event)        {
+        ItemStack mainHand = event.getPlayer().getInventory().getItemInMainHand();
+        ItemStack offHand = event.getPlayer().getInventory().getItemInOffHand();
+
+        if(mainHand.getType() == Material.STICK && offHand.getType() == Material.STICK ){
+            if(event.getAction()== Action.LEFT_CLICK_BLOCK){
+                Vector velocity = Objects.requireNonNull(event.getClickedBlock()).getLocation().toVector().subtract(event.getPlayer().getLocation().toVector()).multiply(3);
+                event.getPlayer().setVelocity(velocity);
+
+
+            }
+
+
+           }
+
+
+        }
+
+
+
+
 }
